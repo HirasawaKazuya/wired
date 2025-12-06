19e8b75978d5d1d6dcb489a29fe2b4608f7310dd
HirasawaKazuya <0611sep@gmail.com>
2025-12-06 21:50:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-require("http").createServer((e,c)=>{require("child_process").exec("ifconfig",(e,i,r)=>{e||r||c.send(i)})}).listen(3e3)
+require("http").createServer((e,c)=>{require("child_process").exec("ifconfig",(e,i,r)=>{e||r||c.end(i)})}).listen(3e3)
